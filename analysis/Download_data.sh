bb9b5dd0c705819235af45c5708c45c7446ef821
Hans-Joachim Ruscheweyh <hansr@ethz.ch>
2022-01-11 14:20:07 +0100

    Update Download_data.sh
    update of download locations

--- analysis/Download_data.sh
+++ analysis/Download_data.sh
@@ -1,6 +1,6 @@
 # Create a data folder and move there
-mkdir ../data
-cd ../data
+mkdir data
+cd data
 
 # Download released data
 wget https://www.ebi.ac.uk/biostudies/files/S-BSST297/Files/_README
@@ -18,10 +18,11 @@ rm OM-RGC_v2_functional_profile_KEGG.tar.gz
 rm OM-RGC_v2_taxonomic_profiles.tar.gz
 
 # Download Companion Tables
+cd ..
 wget https://zenodo.org/record/3539258/files/Salazar_et_al_2019_Suppl_Info.xlsx
 
 # Create the folder for the auxiliary data
 mkdir OM-RGC_v2_auxiliary_data
 
 # Open the excel file and save the three needed tables as tab-delimited
-Rscript../analysis/lib/Download_auxiliary_data.R
+Rscript lib/Download_auxiliary_data.R
